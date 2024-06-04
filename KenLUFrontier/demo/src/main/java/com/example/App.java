d46a6912fe0ac69191b84a1ab47544cff5fed540
KenLuDaDiaoBro <afatfish11@gmail.com>
2024-06-05 05:08:27 +0800

    Front End 2.0

--- KenLUFrontier/demo/src/main/java/com/example/App.java
+++ KenLUFrontier/demo/src/main/java/com/example/App.java
@@ -4,8 +4,8 @@ public class App
 {
     public static void main(String[] args) 
     {
-        LoginFrame frame = new LoginFrame();
-        //MainFrame frame = new MainFrame("Ben");
+        //LoginFrame frame = new LoginFrame();
+        MainFrame frame = new MainFrame("Ben");
         //ItemFrame frame = new ItemFrame("Ben" , 22);
         frame.KenLuakaYBen();
     }
