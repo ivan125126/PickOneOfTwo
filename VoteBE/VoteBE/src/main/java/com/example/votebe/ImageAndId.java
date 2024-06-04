bc1b6610a6ba2dc485b01a89d84985962bd740aa
ivan125126 <ivan125126@gmail.com>
2024-06-05 04:32:05 +0800

    follow function

---
+++ VoteBE/VoteBE/src/main/java/com/example/votebe/ImageAndId.java
@@ -0,0 +1,12 @@
+package com.example.votebe;
+
+public class ImageAndId
+{
+    public String httpString;
+    public Integer IDNumber;
+
+    public ImageAndId(String httpString, Integer IDNumber){
+        this.httpString = httpString;
+        this.IDNumber = IDNumber;
+    }
+}
